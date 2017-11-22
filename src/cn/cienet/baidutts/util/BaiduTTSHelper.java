0c3909e4ae6b83083b0d550ffc6f96434010a646
Aaron <chao_li1699@gmail.com>
2017-11-22 11:11:13 +0800

    add TTSHelper to pro..

---
+++ src/cn/cienet/baidutts/util/BaiduTTSHelper.java
@@ -0,0 +1,167 @@
+package cn.cienet.baidutts.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.baidu.tts.client.SpeechSynthesizer;
+import com.baidu.tts.client.SpeechSynthesizerListener;
+import com.baidu.tts.client.TtsMode;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import android.util.Pair;
+import cn.cienet.baidutts.contral.InitConfig;
+import cn.cienet.baidutts.contral.MySyntherizer;
+import cn.cienet.baidutts.contral.NonBlockSyntherizer;
+import cn.cienet.baidutts.listener.UiMessageListener;
+
+public class BaiduTTSHelper {
+	
+	private static final String TAG = "BaiduTTSHelper";
+	// TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
+    private TtsMode ttsMode = TtsMode.MIX;
+
+    // 离线发音选择，VOICE_FEMALE即为离线女声发音。
+    // assets目录下bd_etts_speech_female.data为离线男声模型；bd_etts_speech_female.data为离线女声模型
+    private String offlineVoice = OfflineResource.VOICE_MALE;
+
+    // ===============初始化参数设置完毕，更多合成参数请至getParams()方法中设置 =================
+
+    // 主控制类，所有合成控制方法从这个类开始
+    private MySyntherizer synthesizer;
+   
+    private volatile static BaiduTTSHelper instance;
+    
+    private BaiduTTSHelper(){}
+
+    public BaiduTTSHelper build(){
+    	
+    	if(instance==null){
+    		synchronized(BaiduTTSHelper.class){
+    			if(instance==null){
+    				instance=new BaiduTTSHelper();
+    			}
+    		}
+    	}
+    	return instance;
+    }
+	
+	/**
+     * 初始化引擎，需要的参数均在InitConfig类里
+     * <p>
+     * DEMO中提供了3个SpeechSynthesizerListener的实现
+     * MessageListener 仅仅用log.i记录日志，在logcat中可以看见
+     * UiMessageListener 在MessageListener的基础上，对handler发送消息，实现UI的文字更新
+     * FileSaveListener 在UiMessageListener的基础上，使用 onSynthesizeDataArrived回调，获取音频流
+     */
+    public void initialTts(Context context, Handler mainHandler, Map<String, String> map) {
+        // 设置初始化参数
+        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler); // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
+
+        Map<String, String> params = getParams();
+
+        // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
+        InitConfig initConfig = new InitConfig(map.get("appId"), map.get("appKey"), map.get("secretKey"), ttsMode, offlineVoice, params, listener);
+
+        synthesizer = new NonBlockSyntherizer(context, initConfig, mainHandler); // 此处可以改为MySyntherizer 了解调用过程
+    }
+
+    /**
+     * 合成的参数，可以初始化时填写，也可以在合成前设置。
+     *
+     * @return
+     */
+    private Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<String, String>();
+        // 以下参数均为选填
+        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0"); // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+        params.put(SpeechSynthesizer.PARAM_VOLUME, "5"); // 设置合成的音量，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_SPEED, "5");// 设置合成的语速，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_PITCH, "5");// 设置合成的语调，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);         // 该参数设置为TtsMode.MIX生效。即纯在线模式不生效。
+        // MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+        // MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
+        return params;
+    }
+
+    /**
+     * speak 实际上是调用 synthesize后，获取音频流，然后播放。
+     * 获取音频流的方式见SaveFileActivity及FileSaveListener
+     * 需要合成的文本text的长度不能超过1024个GBK字节。
+     */
+    public void speak(String text) {
+        int result = synthesizer.speak(text);
+        checkResult(result, "speak");
+    }
+
+
+    /**
+     * 合成但是不播放，
+     * 音频流保存为文件的方法可以参见SaveFileActivity及FileSaveListener
+     */
+    public void synthesize(String text) {
+        int result = synthesizer.synthesize(text);
+        checkResult(result, "synthesize");
+    }
+
+    /**
+     * 批量播放
+     */
+    public void batchSpeak(List<Pair<String, String>> texts) {
+        int result = synthesizer.batchSpeak(texts);
+        checkResult(result, "batchSpeak");
+    }
+
+
+    /**
+     * 切换离线发音。注意需要添加额外的判断：引擎在合成时该方法不能调用
+     */
+    public void loadModel() {
+        if (offlineVoice.equals(OfflineResource.VOICE_FEMALE)) {
+            offlineVoice = OfflineResource.VOICE_MALE;
+        } else {
+            offlineVoice = OfflineResource.VOICE_FEMALE;
+        }
+        int result = synthesizer.loadModel(offlineVoice);
+        checkResult(result, "loadModel");
+    }
+
+    private void checkResult(int result, String method) {
+        if (result != 0) {
+            Log.e(TAG, "error code :" + result + " method:" + method + ", 错误码文档:http://yuyin.baidu.com/docs/tts/122 ");
+        }
+    }
+
+
+    /**
+     * 暂停播放。仅调用speak后生效
+     */
+    public void pause() {
+        int result = synthesizer.pause();
+        checkResult(result, "pause");
+    }
+
+    /**
+     * 继续播放。仅调用speak后生效，调用pause生效
+     */
+    public void resume() {
+        int result = synthesizer.resume();
+        checkResult(result, "resume");
+    }
+
+    /*
+     * 停止合成引擎。即停止播放，合成，清空内部合成队列。
+     */
+    public void stop() {
+        int result = synthesizer.stop();
+        checkResult(result, "stop");
+    }
+    
+    public void release(){
+    	synthesizer.release();
+    }
+}
